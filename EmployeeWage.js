dbeb17f0314604ca27bee27668c1c60fd8450d81
harshhoda160319 <harshgood07@gmail.com>
2025-06-10 15:19:37 +0530

    UC1 : check emp attendance..

---
+++ EmployeeWage.js
@@ -0,0 +1,13 @@
+//Use Case :1 Will Check if an employee is present
+
+const present = 1;
+let attendance = Math.floor(Math.random() * 2); 
+
+if (attendance == present) {
+
+  console.log("Employee is Present");
+}
+ else {
+
+  console.log("Employee is Absent");
+}
